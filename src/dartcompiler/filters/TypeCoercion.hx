00ac68c116c5e23bb413b671210c475cd876755f
fourst4r <oxygenmagnet@hotmail.com>
2024-02-23 00:14:13 +1300

    coerce int to double where necessary

---
+++ src/dartcompiler/filters/TypeCoercion.hx
@@ -0,0 +1,52 @@
+package dartcompiler.filters;
+
+#if (macro || dart_runtime)
+
+import haxe.macro.Type;
+import reflaxe.helpers.Context;
+
+class TypeCoercion implements IExprFilter implements IClassFilter {
+    public function new() {}
+
+    static function ref<T>(o:T) return {get: () -> o, toString: Std.string.bind(o)};
+
+    static function mk(def:TypedExprDef, ?t:Type) {
+        return TypedExprHelper.make(def, t ?? TDynamic(null), null);
+    }
+
+    /**
+        We have to do this specifically for returns, because for some reason
+        TReturn type is always dynamic so we can't handle it in filterExpr...
+    **/
+    public function filterClass(cls:ClassDef):ClassDef {
+        for (func in cls.funcFields) {
+            if (func.ret.isFloat()) {
+                func.setExpr(coerceReturnsToDouble(func.expr));
+            }
+        }
+        return cls;
+    }
+
+    function coerceReturnsToDouble(e:TypedExpr):TypedExpr {
+        return switch (e.expr) {
+            case TReturn(re): return e.copy(TReturn(mk(TCall(mk(TField(re, FDynamic("toDouble"))), []))));
+            default: e.map(coerceReturnsToDouble);
+        }
+    }
+
+    function coerceToDouble(e:TypedExpr):TypedExpr {
+        return mk(TCall(mk(TField(e, FDynamic("toDouble"))), []));
+    }
+
+    public function filterExpr(e:TypedExpr):TypedExpr {
+        // ((i:Int):Float) ==> i.toDouble()
+        return switch (e.expr) {
+            case TBinop(OpAssign, e1, e2) if (e1.t.isFloat() && e2.t.isInt()):
+                e.copy(TBinop(OpAssign, e1, coerceToDouble(e2)));
+            default:
+                e;
+        }
+    }
+}
+
+#end
