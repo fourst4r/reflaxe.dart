b24c10b04c781500e561e64ed92e908f587aa574
fourst4r <oxygenmagnet@hotmail.com>
2024-02-28 23:10:23 +1300

    be safe

--- std/dart/HaxeIterator.hx
+++ std/dart/HaxeIterator.hx
@@ -1,12 +1,16 @@
 package dart;
 
+@:nullSafety(Strict)
 final class HaxeIterator<E> {
     var _it:dart.core.Iterator<E>;
     var _hasNext:Null<Bool>;
+    
     public function new(it:dart.core.Iterator<E>) 
         _it = it;
+    
     public function hasNext():Bool
         return _hasNext ??= _it.moveNext();
+
     public function next():E {
         if (_hasNext ??= _it.moveNext()) {
             _hasNext = null;
